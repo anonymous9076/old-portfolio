d05307b617f73a171cc0bfdc1718f591523aad4c
Tushar kumar <68018920+anonymous9076@users.noreply.github.com>
2023-10-09 22:04:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Sec4 from './Components/Sec4';
 // import Contact from './Components/Contact';
 function App() {
     return (
-        < >
+        <div className="mainbody" >
         <Sec1></Sec1>
         <Sec2></Sec2>
         <Sec3></Sec3>
         <Sec4></Sec4>
         {/* <Contact></Contact> */}
-        </>
+        </div>
 
     )
 }
