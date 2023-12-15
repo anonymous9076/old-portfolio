c3a8e25574c2b9d7526638ecece56fe2eeadecb9
anonymous9076 <vijaytushar915@gmail.com>
2023-12-15 20:46:07 +0530

    latest-dec15

--- src/Components/Sec3.js
+++ src/Components/Sec3.js
@@ -13,9 +13,9 @@ const Sec3 = () => {
                     When discussing my projects, I can highlight my experience in handling a total of three projects, including two minor projects and one major project. Currently, I am actively involved in a significant e-commerce project, which has been a valuable opportunity to apply my skills and knowledge in a real-world setting. This project involves the development of a dynamic and user-friendly e-commerce website, where I have been responsible for tasks such as front-end design, database management, and ensuring smooth functionality. Through these experiences, I have honed my web development skills, including proficiency in HTML, CSS, JavaScript, and database management systems. These projects have not only enhanced my technical capabilities but have also improved my problem-solving and teamwork skills, as effective collaboration is crucial in delivering successful projects.
                 </div>
                 <div className='proj-item'>
-                    <Link id='proj-img' to='http://piratestore.netlify.app' target='_blank'>  <img  src='./images/pirate.png' alt='...'></img></Link>
+                    <Link id='proj-img' to='http://piratestore.netlify.app' target='_blank'>  <img  src='./images/pirate.jpg' alt='...'></img></Link>
                     <Link id='proj-img' to='http://osm-mosam.netlify.app' target='_blank'>  <img src='./images/mosam.png' alt='...'></img></Link>
-                    <Link id='proj-img' to='http://icey-food.netlify.app' target='_blank'>  <img src='./images/iceweb.png' alt='...'></img></Link>
+                    <Link id='proj-img' to='http://icey-food.netlify.app' target='_blank'>  <img src='./images/iceweb.jpg' alt='...'></img></Link>
                     <Link id='proj-img' to='http://tushar1292.netlify.app' target='_blank'>  <img src='./images/mypage.png' alt='...'></img></Link>
 
                 </div>
