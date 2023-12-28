dfc1cff8f8ed0971e4576ed9c33648c97dfafefe
anonymous9076 <vijaytushar915@gmail.com>
2023-12-29 01:48:06 +0530

    yoest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
-import Sec1 from './components/Sec1.js'
-import Sec2 from './components/Sec2.js'
-import Sec3 from './components/Sec3.js'
-import Sec4 from './components/Sec4.js'
-import Sec5 from './components/Sec5.js'
+import Sec1 from './Components/Sec1.js'
+import Sec2 from './Components/Sec2.js'
+import Sec3 from './Components/Sec3.js'
+import Sec4 from './Components/Sec4.js'
+import Sec5 from './Components/Sec5.js'
 
 import { BrowserRouter } from 'react-router-dom'
 
