01cf5323f727434f5b865e23460f135749f70dc3
anonymous9076 <vijaytushar915@gmail.com>
2023-12-29 01:46:47 +0530

    yoest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import './App.css'
-import Sec1 from './components/Sec1'
-import Sec2 from './components/Sec2'
-import Sec3 from './components/Sec3'
-import Sec4 from './components/Sec4'
-import Sec5 from './components/Sec5'
+import Sec1 from './components/Sec1.js'
+import Sec2 from './components/Sec2.js'
+import Sec3 from './components/Sec3.js'
+import Sec4 from './components/Sec4.js'
+import Sec5 from './components/Sec5.js'
 
-import { BrowserRouter,Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className='app'>
       <BrowserRouter>
-      <Route>
       <Sec1></Sec1>
       <Sec2></Sec2>
       <Sec3></Sec3>
       <Sec4></Sec4>
       <Sec5></Sec5>
-      </Route>
       </BrowserRouter>
     </div>
   )
