7cf859c449ab66f39e5df6047edd982943fa456f
anonymous9076 <vijaytushar915@gmail.com>
2023-12-30 00:54:56 +0530

    latest update

--- src/Components/Sec5.js
+++ src/Components/Sec5.js
@@ -6,29 +6,31 @@ function Sec5() {
     <div className='sec5'>
         <h1 className='colorful'> Contact Me :</h1>
         <div className='contact-box'>
-            <div className='cont-area'>
+           
+                <form  className='cont-area' action='https://formspree.io/f/xnqeqnad' method='post'>
                 <span className='enter-field'>
                     <label>First Name</label><br></br>
-                    <input placeholder='Enter your firstname' ></input>
+                    <input name='firstname' placeholder='Enter your firstname' ></input>
                 </span>
                 <span className='enter-field'>
                     <label>Last Name</label><br></br>
-                    <input placeholder='Enter your lastname'></input>
+                    <input name='lastname' placeholder='Enter your lastname'></input>
                 </span>
                 <span className='enter-field'>
                     <label>E-mail</label><br></br>
-                    <input placeholder='Enter your email'></input>
+                    <input name='email' placeholder='Enter your email'></input>
                 </span>
                 <span className='enter-field'>
                     <label>Phone-No.</label><br></br>
-                    <input placeholder='Enter your number'></input>
+                    <input name='phn-no.' placeholder='Enter your number'></input>
                 </span>
                 <span className='enter-field'>
                     <label>Your Query</label><br></br>
-                    <textarea placeholder='Enter your query'></textarea>
+                    <textarea name='message' placeholder='Enter your query'></textarea>
                 </span>
                 <span className='enter-field for-btn'><button type='submit' className='sb-btn'>Submit</button></span>
-            </div>
+                </form>
+            
         </div>
     </div>
   )
